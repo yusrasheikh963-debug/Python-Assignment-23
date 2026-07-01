ab19b67344d7a73aab523ba4a3b5b08664f07884
yusrasheikh963-debug <yusrasheikh963@gmail.com>
2026-07-01 12:13:00 +0530

    Complete basic array creation

---
+++ Assignment 13/Que1.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50])   # Creating a 1D array
+print("1D array:", arr)   # Print a 1D array
+print("Shape of 1D array:", arr.shape)   # Print the shape of 1D array
+print("data type of 1D array:", arr.dtype)  # Print the data type of 1D array
+
+arr2 = np.array([[1, 2, 3], [4, 5, 6]])  # Creating a 2D array
+print("\n2D array:", arr2)   # Print a 2D array
+print("Shape of 2D array:", arr2.shape)   # Print the shape of 2D array
+print("data type of 2D array:", arr2.dtype)  # Print the data type of 2D array
